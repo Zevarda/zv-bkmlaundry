02376f4bd7b09aeb233bd7c74bb48a442af1df41
Zevarda <khoironisubagio01@gmail.com>
2025-05-12 21:38:23 +0700

    main script

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Money Laundering System - Auto Target & Framework'
+author 'zDev'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    '@qb-core/shared/locale.lua',
+    'config.lua'
+}
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server.lua'
+}
